6d67b2a3e3827d7d0cb9ea986f942fc64eb14ed2
Sönke Hahn <soenkehahn@gmail.com>
2019-05-28 14:21:18 -0400

    shut down pixel 2 bluetooth connection before suspending

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,13 @@ use std::thread;
 use std::time::Duration;
 
 fn main() -> Result<(), Box<Error>> {
+    Command::new("nmcli")
+        .args(vec!["connection", "down", "Pixel 2 Network"])
+        .output()?;
     let mut lock_command = Command::new("i3lock").spawn()?;
-    thread::sleep(Duration::from_millis(100));
+    thread::sleep(Duration::from_millis(200));
     let mut suspend_command = Command::new("sudo").arg("pm-suspend").spawn()?;
     lock_command.wait()?;
     suspend_command.wait()?;
     Ok(())
 }
-// i3lock & (sleep 0.1 ; sudo pm-suspend)
