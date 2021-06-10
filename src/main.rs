153c41da3143398765b8b8d79d70a1aec952c4b2
Sönke Hahn <soenkehahn@gmail.com>
2021-06-09 21:58:25 -0400

    Switch to darker color

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
+use cradle::*;
 use std::error::Error;
 use std::process::Command;
 use std::thread;
 use std::time::Duration;
 
-fn main() -> Result<(), Box<Error>> {
-    Command::new("nmcli")
-        .args(vec!["connection", "down", "Pixel 2 Network"])
-        .output()?;
-    let mut lock_command = Command::new("i3lock").spawn()?;
+fn main() -> Result<(), Box<dyn Error>> {
+    let Exit(_) = cmd_result!(%"nmcli connection down", "Pixel 2 Network")?;
+    let mut lock_command = Command::new("i3lock").args(vec!["-c", "222222"]).spawn()?;
     thread::sleep(Duration::from_millis(200));
     let mut suspend_command = Command::new("sudo").arg("pm-suspend").spawn()?;
     lock_command.wait()?;
